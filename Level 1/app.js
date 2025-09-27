e547829f7fe31c476bf3a2bfae1606ae659743ad
sujalbirari <sujalbirari@gmail.com>
2025-09-27 13:25:03 +0530

    ques1 - part2 - Adding Rate Limiter Middleware

--- Level 1/app.js
+++ Level 1/app.js
@@ -14,7 +14,7 @@ async function loggerMiddleware(req, res, next) {
         const data = ['req method: ' + req.method, 'req url: ' + req.url, 'req timestamp: ' + timestamp, 'req ip address: ' + req.ip];
         const content = data.join('\n');
 
-        await fs.writeFile('requests.log', content, 'utf-8');
+        await fs.appendFile('requests.log', content, 'utf-8');
         next();
     }
     catch (err) {
@@ -22,6 +22,28 @@ async function loggerMiddleware(req, res, next) {
     }
 }
 
+let clientTrack = {};
+// 100 requests per 1 minute per ip address
+function rateLimiter(req, res, next) {
+    const ONE_MINUTE = 60 * 1000;
+
+    const clientIP = req.ip;
+    const now = Date.now();
+    if (!clientTrack[clientIP]) {
+        clientTrack[clientIP] = [];
+    }
+    clientTrack[clientIP] = clientTrack[clientIP].filter(ts => now - ts < ONE_MINUTE);
+    clientTrack[clientIP].push(now);
+    if (clientTrack[clientIP].length > 100) {
+        res.set('Retry-After', '60');
+        res.status(429).send('Too many requests');
+        return;
+    }
+
+    next();
+}
+
+app.use(rateLimiter);
 app.use(loggerMiddleware);
 
 app.get('/', (req, res) => {
